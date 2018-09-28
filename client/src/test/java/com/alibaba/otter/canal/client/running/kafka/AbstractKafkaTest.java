25642664d453f56574a6b75a588c6120cc0d935d
huyang <i@huyang.me>
2018-09-28 19:48:10 +0800

    fix 修改获取kafka消息测试类

--- client/src/test/java/com/alibaba/otter/canal/client/running/kafka/AbstractKafkaTest.java
+++ client/src/test/java/com/alibaba/otter/canal/client/running/kafka/AbstractKafkaTest.java
@@ -13,8 +13,8 @@ public abstract class AbstractKafkaTest {
     public static String  topic     = "example";
     public static Integer partition = 1;
     public static String  groupId   = "g4";
-    public static String  servers   = "10.135.248.214:9092,10.135.248.226:9092,10.30.141.93:9092";
-    public static String  zkServers = "10.135.248.214:2181,10.135.248.226:2181,10.30.141.93:2181";
+    public static String  servers   = "10.111.10.250:9092,10.111.10.249:9092,10.111.10.251:9092";
+    public static String  zkServers = "10.111.10.250:2181,10.111.10.249:2181,10.111.10.251:2181";
 
     public void sleep(long time) {
         try {
